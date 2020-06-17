965234c4f9e6c0a997014786dd171eef7d7dbff5
lttt1703 <60804574+lttt1703@users.noreply.github.com>
2020-06-18 01:46:15 +0700

    1:46

--- BigSchool/Migrations/202006040656461_PopulateCategoryTable.cs
+++ BigSchool/Migrations/202006040656461_PopulateCategoryTable.cs
@@ -10,6 +10,10 @@ namespace BigSchool.Migrations
             Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (1,'Development')");
             Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (2,'Business')");
             Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (3,'Marketing')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (4,'BoChinSu')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (5,'IT')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (6,'Banking')");
+            Sql("INSERT INTO CATEGORIES (ID, NAME) VALUES (7,'Photo')");
         }
         
         public override void Down()
